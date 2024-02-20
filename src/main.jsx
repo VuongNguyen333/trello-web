4acf9052f3c411fd1ed0c963087890798981a3a9
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-02-20 15:06:53 +0700

    set-dark-light-mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import theme from './theme'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </React.StrictMode>
 )
