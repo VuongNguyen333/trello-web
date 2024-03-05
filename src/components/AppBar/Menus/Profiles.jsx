dd27140b2079150c4e698fe7c28bef9c6d4edca4
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-05 23:57:53 +0700

    appBar-p2.1

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -33,7 +33,7 @@ function Profiles() {
           <Avatar
             sx={{ width: 30, height: 30 }}
             alt='Vuong Dev'
-            src='https://scontent.fhan5-9.fna.fbcdn.net/v/t39.30808-6/428613184_777882731041092_5204124655893914184_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHqUZ5cUSXylrivRbUjkDyxEso5hltSZf8SyjmGW1Jl_2CoUeIvsWOWn8KlwtKAFbbdRhY7IZADdc3MBkGMafuW&_nc_ohc=-31aIxv3638AX_Rnqvj&_nc_ht=scontent.fhan5-9.fna&oh=00_AfBXlH1g27yrwkFYmQEO0WueBPAF2QGmugb2QEl_twXQxQ&oe=65ECAEC7'
+            src='https://avatars.githubusercontent.com/u/108895030?s=400&u=a4a2dfeb897eb7e4e5a5125150306653774c9f9b&v=4'
           />
         </IconButton>
       </Tooltip>
@@ -50,14 +50,14 @@ function Profiles() {
           <Avatar
             sx={{ width: 28, height: 28, mr: 1.2 }}
             alt='Vuong Dev'
-            src='https://scontent.fhan5-9.fna.fbcdn.net/v/t39.30808-6/428613184_777882731041092_5204124655893914184_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHqUZ5cUSXylrivRbUjkDyxEso5hltSZf8SyjmGW1Jl_2CoUeIvsWOWn8KlwtKAFbbdRhY7IZADdc3MBkGMafuW&_nc_ohc=-31aIxv3638AX_Rnqvj&_nc_ht=scontent.fhan5-9.fna&oh=00_AfBXlH1g27yrwkFYmQEO0WueBPAF2QGmugb2QEl_twXQxQ&oe=65ECAEC7'
+            src='https://avatars.githubusercontent.com/u/108895030?s=400&u=a4a2dfeb897eb7e4e5a5125150306653774c9f9b&v=4'
           /> Profile
         </MenuItem>
         <MenuItem>
           <Avatar
             sx={{ width: 28, height: 28, mr: 1.2 }}
             alt='Vuong Dev'
-            src='https://scontent.fhan5-9.fna.fbcdn.net/v/t39.30808-6/428613184_777882731041092_5204124655893914184_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHqUZ5cUSXylrivRbUjkDyxEso5hltSZf8SyjmGW1Jl_2CoUeIvsWOWn8KlwtKAFbbdRhY7IZADdc3MBkGMafuW&_nc_ohc=-31aIxv3638AX_Rnqvj&_nc_ht=scontent.fhan5-9.fna&oh=00_AfBXlH1g27yrwkFYmQEO0WueBPAF2QGmugb2QEl_twXQxQ&oe=65ECAEC7'
+            src='https://avatars.githubusercontent.com/u/108895030?s=400&u=a4a2dfeb897eb7e4e5a5125150306653774c9f9b&v=4'
           /> My account
         </MenuItem>
         <Divider />
