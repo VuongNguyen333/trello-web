9eb9491d7650a28aa773b5d72cfe5c4957f8c3eb
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-13 23:44:31 +0700

    color-theme-p1

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -3,6 +3,7 @@ import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 import { useColorScheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
@@ -27,12 +28,12 @@ function ModeSelect() {
       >
         <MenuItem value="dark">
           <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <LightModeIcon fontSize='samll' ></LightModeIcon> Dark
+            <DarkModeIcon fontSize='samll' ></DarkModeIcon> Dark
           </Box>
         </MenuItem>
         <MenuItem value="light">
           <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            Light
+            <LightModeIcon fontSize='samll' ></LightModeIcon> Light
           </Box>
         </MenuItem>
         <MenuItem value="system">
