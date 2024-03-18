8f82d4c4bd1e984ec0b2a4696a1ef94e79504bc5
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-18 17:18:59 +0700

    dnd-overlay

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -21,7 +21,7 @@ export const mockData = {
             columnId: 'column-id-01',
             title: 'Title of card 01',
             description: 'Markdown Syntax ',
-            cover: 'https://trungquandev.com/wp-content/uploads/2022/07/fair-mern-stack-advanced-banner-trungquandev.jpg',
+            cover: 'https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/hinh-nen-may-tinh-dep-a-35-1.jpg',
             memberIds: ['test-user-id-01'],
             comments: ['test comment 01', 'test comment 02'],
             attachments: ['test attachment 01', 'test attachment 02', 'test attachment 03']
