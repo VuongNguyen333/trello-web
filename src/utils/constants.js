c97519a2c7aac8ad5e3624a27f847208829617ae
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-23 17:06:23 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,13 @@
-export const API_ROOT = 'https://trello-api-seft.onrender.com'
+/* eslint-disable no-undef */
+let apiRoot = ''
+console.log(import.meta.env)
+console.log(process.env)
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-seft.onrender.com'
+}
+
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  // cho phep vite su dung process.env
+  // https://github.com/vitejs/vite/issues/1973
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
